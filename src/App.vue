7c1beb0072d43ba35396c61ff9f98d60e66fa128
Sebasthian von Bergen <sgvonbergen@uc.cl>
2024-05-05 13:12:53 -0400

    Created websocket, Setup GET requests

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,51 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "./components/HelloWorld.vue";
+import { provide } from "vue";
+
+const apiUrl = "https://tarea-2.2024-1.tallerdeintegracion.cl/api/metro";
+provide("apiUrl", apiUrl);
+
+const webSocketServerUrl = "wss://tarea-2.2024-1.tallerdeintegracion.cl/connect";
+const webSocket = new WebSocket(webSocketServerUrl);
+webSocket.onopen = (event) => console.log(event);
+webSocket.eventTypes = ["position", "status", "arrival", "departure", "boarding", "unboarding", "message"];
+webSocket.eventHandlers = {};
+webSocket.eventTypes.forEach(eventType => {
+    webSocket.eventHandlers[eventType] = (event) => console.log(event);
+});
+webSocket.onmessage = (event) => {
+    if (event.type in webSocket.eventTypes) {
+        let eventHandler = webSocket.eventHandlers[event.type];
+        eventHandler(event);
+    } else {
+        console.log(event);
+    }
+}
+
+provide("webSocket", webSocket);
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <header>
+        <img
+            alt="Vue logo"
+            class="logo"
+            src="@/assets/logo.svg"
+            width="125"
+            height="125"
+        >
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+        <div class="wrapper">
+            <HelloWorld msg="You did it!" />
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+            <nav>
+                <RouterLink to="/">Home</RouterLink>
+                <RouterLink to="/about">About</RouterLink>
+            </nav>
+        </div>
+    </header>
 
-  <RouterView />
+    <RouterView />
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
---
+++ src/components/StationList.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { provide, inject, ref, onMounted } from "vue";
+import axios from "axios";
+
+const stations = ref([]);
+const loading = ref(true);
+const apiUrl = inject("apiUrl");
+async function loadStations() {
+    return axios.get(apiUrl + "/stations")
+}
+async function loadLines() {
+    return axios.get(apiUrl + "/lines")
+}
+async function loadTrains() {
+    return axios.get(apiUrl + "/trains")
+}
+onMounted(async () => {
+    const [st, li, tr] = await Promise.all([
+        loadStations(),
+        loadLines(),
+        loadTrains()
+    ])
+    
+})
+
+</script>
